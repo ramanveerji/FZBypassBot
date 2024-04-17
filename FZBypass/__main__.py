4bff3855aaa3cbe9bf04110966bde062a50ab3e1
RamandeepS <70764724+ramanveerji@users.noreply.github.com>
2024-04-18 02:23:48 +0530

    Update __main__.py

--- FZBypass/__main__.py
+++ FZBypass/__main__.py
@@ -28,7 +28,7 @@ async def restart():
 
 
 Bypass.start()
-LOGGER.info("FZ Bot Started!")
+LOGGER.info('RS Link Bypasser Bot Started!')
 Bypass.loop.run_until_complete(restart())
 idle()
 Bypass.stop()
